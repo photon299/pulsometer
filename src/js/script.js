94f775c58a46c18eec61621155f3cc4d918402de
Alex <alex.real1850@gmail.com>
2021-11-14 00:42:53 +0200

    pulse saturday

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: true,
-        prevArrow:'<button type="button" class="slick-prev"><img src="../icons/slider_arrows/arrow_left.png"></button>'
+        // adaptiveHeight: true,  
+        prevArrow:'<button type="button" class="slick-prev"><img src="icons/slider_arrows/arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/slider_arrows/arrow_right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                  dots: true,
+                  arrows: false
+                }
+            }   
+        ]
       });
   });
